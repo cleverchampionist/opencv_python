97d9f8a257e5579155b045281f7a19b9903f0be1
vasyl <vasyl.savuliak@outlook.com>
2023-10-24 19:57:20 +0200

    add one folder name in .gitignore

--- image.py
+++ image.py
@@ -3,6 +3,9 @@ import cv2
 
 #load a color image in grayscale
 img = cv2.imread('OpenCV_Logo.png', 1)
+
 cv2.imshow('image', img)
-cv2.waitKey(0)
+key = cv2.waitKey(0)
+if key == ord('s'):
+    cv2.imwrite("opencv_logo_GS.png", img)
 cv2.destroyAllWindows()
